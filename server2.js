aee31b1ae500d1898e97b98ebdfe936f3f6cf0d6
NhuongPH <amslaypham@gmail.com>
2018-05-28 22:33:42 +0700

    re write server2

--- server2.js
+++ server2.js
@@ -10,21 +10,12 @@ let data = XLSX.utils.sheet_to_json(sheet1, {header: 1, range: 3});
 let $count = 1;
 
 let header = data[0][0];
-FS.unlink(PATH, (err) => {
-  if (err) throw err;
-  appendToFile(PATH, header);
-  delete data[0];
+FS.writeFileSync(PATH, header, 'utf8');
+delete data[0];
 
-  data.forEach(function (row) {
-    if (row[0]) {
-      data = "\n" + $count++ + '. ' + row[0];
-      appendToFile(PATH, data);
-    }
-  });
-})
-
-function appendToFile (path, data, encode = 'utf8') {
-  FS.appendFileSync(path, data, encode, function (err) {
-    if (err) throw err;
-  });
-}
+data.forEach(function (row) {
+  if (row[0]) {
+    data = "\n" + $count++ + '. ' + row[0];
+    FS.appendFileSync(PATH, data, 'utf8');
+  }
+});
